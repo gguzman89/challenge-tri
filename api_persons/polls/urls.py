78a09181d43a3a185baa51080a9f93c88e70e34d
gguzman89 <ggtpi0gabriel@gmail.com>
2020-01-11 21:38:16 -0300

    add api gets

--- api_persons/polls/urls.py
+++ api_persons/polls/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('courses/', views.courses, name='courses'),
+    path('languages/', views.languages, name='languages'),
+    path('levels/', views.levels, name='levels'),
+    path('peoples/', views.people_new, name='people_new'),
+    path('peoples/<int:person_id>/', views.people_find, name='people_find'),
+
 ]
--- api_persons/polls/views.py
+++ api_persons/polls/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+import requests
+from .models import People, Level, Language, Course
 
 
 def home(request):
     return HttpResponse('Hello World!')
+
+
+def people_new(request):
+    # if request.method == 'POST':
+    #     name = request.POST['first_name']
+    #     last_name = request.POST['last_name']
+    #     email = request.POST['email']
+    #
+    #     person = People.objects.create(
+    #         first_name=name,
+    #         last_name=last_name,
+    #         email=email
+    #     )
+    # return HttpResponse('New person added...')
+    return HttpResponse(People.objects.all())
+
+
+def _url(path):
+    return 'http://localhost:8000/api/' + path
+
+
+def courses(request):
+    return HttpResponse(Course.objects.all())
+
+
+def languages(request):
+    return HttpResponse(Language.objects.all())
+
+
+def levels(request):
+    return HttpResponse(Level.objects.all())
+
+
+def people_find(request, person_id):
+    people = get_object_or_404(People, pk=person_id)
+    return HttpResponse(people)
